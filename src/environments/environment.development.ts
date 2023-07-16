9d3c8a7f2c891915f4c78e8f8926123507b49846
11Samo <samuel.kurnas@gmail.com>
2023-07-16 10:24:24 +0200

    Prepare for hosting

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  firebaseAPIKey: 'AIzaSyA7Owu0jd2sj5y6X9kYSPFLbkf8dgC15Qk',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyA7Owu0jd2sj5y6X9kYSPFLbkf8dgC15Qk',
+};
